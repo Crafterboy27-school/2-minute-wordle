40ce784806efe1f1e8d4900406f3dc330095e007
Crafterboy27-school <95499245+Crafterboy27-school@users.noreply.github.com>
2024-05-14 09:52:48 -0500

    Update script.js

--- script.js
+++ script.js
@@ -25,7 +25,7 @@
 
 
   async function game() {
-    let startDate = new Date(2024, 4, 14);
+    let startDate = new Date(2024, 5, 13);
     let lastMinutes = 0 - Math.floor((new Date() - startDate) / (1000 * 60 * 2));
     console.log(lastMinutes)
 
